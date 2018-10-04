d364739ab1f915e6cf74330aa83b5b04e75f45dc
youichiro <cinnamon416@gmail.com>
2018-10-04 12:00:25 +0900

    fix

--- test.py
+++ test.py
@@ -33,9 +33,7 @@ def main():
     dropout = opts['dropout']
 
     # model
-    left_encoder = nets.RNNEncoder(n_vocab, n_units, n_layer, dropout)
-    right_encoder = nets.RNNEncoder(n_vocab, n_units, n_layer, dropout)
-    model = nets.ContextClassifier(left_encoder, right_encoder, n_class)
+    model = nets.ContextClassifier(n_vocab, n_units, n_class)
     chainer.serializers.load_npz(args.model, model)
     if args.gpuid >= 0:
         cuda.get_device(args.gpuid).use()
