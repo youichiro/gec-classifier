bf8ebe5a1d0cc20fcacdc805db639b6691401190
youichiro <cinnamon416@gmail.com>
2019-06-12 12:52:03 +0900

    fix

--- src/test_particle19.py
+++ src/test_particle19.py
@@ -61,8 +61,8 @@ def main():
         f_hyp_char.close()
 
     # m2scoreを実行
-    call = f"python /lab/ogawa/tools/m2score/m2score {save_file_char} {args.save_dir}/naist_test19_char.m2"
-    call_output = subprocess.check_output(call.split(), shell=True)
+    call = f"python /lab/ogawa/tools/m2scorer/m2scorer {save_file_char} {args.save_dir}/naist_test19_char.m2"
+    call_output = subprocess.check_output(call, shell=True)
     print(call_output)
 
 
