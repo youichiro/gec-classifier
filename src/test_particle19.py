9b3c6af28655a9c3e0372a53e11d33145653efd3
youichiro <cinnamon416@gmail.com>
2019-06-12 13:15:46 +0900

    fix

--- src/test_particle19.py
+++ src/test_particle19.py
@@ -14,8 +14,7 @@ def main():
     parser.add_argument('--ans', required=True, help='Answer corpus')
     parser.add_argument('--reverse', default=False, action='store_true',
                         help='Whether to reverse the prediction order')
-    parser.add_argument('--show', default=False, action='store_true', help='Whether to show results')
-    parser.add_argument('--save-dir', default='results/v2', help='Save directory')
+    parser.add_argument('--save-dir', required=True, help='Save directory')
     args = parser.parse_args()
 
     mecab_dict_file = os.environ['MECABDIC']
@@ -27,38 +26,30 @@ def main():
     error_data = open(args.err).readlines()
     answer_data = open(args.ans).readlines()
 
-    if args.save_dir:
-        save_dir = args.save_dir + '/' + args.model_dir.split('/')[-1]
-        os.makedirs(save_dir, exist_ok=True)
-        save_file = save_dir + '/model.hyp'
-        f_hyp = open(save_file, 'w')
-        save_file_char = save_dir + '/model_char.hyp'
-        f_hyp_char = open(save_file_char, 'w')
+    save_dir = args.save_dir + '/' + args.model_dir.split('/')[-1]
+    os.makedirs(save_dir, exist_ok=True)
+    save_file = save_dir + '/model.hyp'
+    f_hyp = open(save_file, 'w')
+    save_file_char = save_dir + '/model_char.hyp'
+    f_hyp_char = open(save_file_char, 'w')
+    save_file_out = save_dir + '/model.out'
+    f_hyp_out = open(save_file_out, 'w')
 
-    if not args.show:
-        error_data = tqdm(error_data)
-
-    for i, (err, ans) in enumerate(zip(error_data, answer_data)):
+    for i, (err, ans) in enumerate(zip(tqdm(error_data), answer_data)):
         err = err.replace('\n', '')
         ans = ans.replace('\n', '')
         hyp = checker.correction(err)
 
-        if args.show:
-            print(f'--- {i} ---')
-            print(f'err: {err}')
-            print(f'hyp: {hyp}')
-            print(f'ans: {ans}')
-            print()
+        f_hyp.write(hyp + '\n')
+        f_hyp_char.write(' '.join(hyp) + '\n')
+        f_hyp_out.write(f'--- {i} ---\nerr: {err}\nhyp: {hyp}\nans: {ans}\n\n')
 
-        if args.save_dir:
-            f_hyp.write(hyp + '\n')
-            f_hyp_char.write(' '.join(hyp) + '\n')
-
-    if args.save_dir:
-        print('Saved to ' + save_file)
-        print('Saved to ' + save_file_char)
-        f_hyp.close()
-        f_hyp_char.close()
+    print('Saved to ' + save_file)
+    print('Saved to ' + save_file_char)
+    print('Saved to ' + save_file_out)
+    f_hyp.close()
+    f_hyp_char.close()
+    f_hyp_out.close()
 
     # m2scoreを実行
     cmd = f"python /lab/ogawa/tools/m2scorer/m2scorer {save_file_char} {args.save_dir}/naist_test19_char.m2 > {save_dir}/m2score.txt"
