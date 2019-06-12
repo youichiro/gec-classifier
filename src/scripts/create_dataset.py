8d9485525278cbdd7ee2d1b5e85cad56accbeadc
youichiro <cinnamon416@gmail.com>
2019-06-12 19:13:30 +0900

    fix

--- src/scripts/create_dataset.py
+++ src/scripts/create_dataset.py
@@ -49,6 +49,41 @@ def get_del_positions(words, parts):
     return del_idx
 
 
+def make_labeled_sentence(text, args):
+    line = clean_text(line.rstrip())  # クリーニング
+    words, parts = mecab.tagger(line)  # 形態素解析
+    words, parts = mecab.preprocessing_to_particle(words, parts, TARGETS, TARGET_PARTS)  # 2単語になった助詞を1単語に変換しておく
+    target_idx = get_target_positions(words, parts)  # 助詞の位置を検出
+    del_idx = get_del_positions(words, parts)  # 削除ラベルを挿入する位置を検出
+    n_target = len(target_idx) + len(del_idx)
+
+    # ラベル付けする位置を決める
+    if n_target == 0 or len(words) > args.maxlen:
+        continue
+    elif len(target_idx) == 0:
+        target_id = random.choice(del_idx)
+    elif len(del_idx) == 0:
+        target_id = random.choice(target_idx)
+    else:
+        # 文中に複数対象がある場合はランダムに1箇所選ぶ
+        # 削除ラベルをX%の確率で作成する
+        if random.random() < float(args.del_rate):
+            target_id = random.choice(del_idx)
+        else:
+            target_id = random.choice(target_idx)
+
+    # ラベル付け
+    if parts[target_id][:2] == '助詞' or parts[target_id] == '助動詞':
+        labeled_sentence = '{} <{}> {}'.format(
+            ' '.join(words[:target_id]), words[target_id], ' '.join(words[target_id+1:]))
+    else:
+        # 削除ラベルの場合
+        labeled_sentence = '{} <{}> {}'.format(
+            ' '.join(words[:target_id]), 'DEL', ' '.join(words[target_id:]))
+
+    return labeled_sentence
+
+
 def main():
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('--corpus', required=True, help='Corpus file')
@@ -60,53 +95,61 @@ def main():
     parser.add_argument('--del-rate', default=0.1, help='Rate of deletion label (0.0~1.0)')
     args = parser.parse_args()
 
-    mecab = Mecab(args.mecab_dic)
-    count = 0
-    if os.path.exists(args.save_train):
-        os.remove(args.save_train)
-    if os.path.exists(args.save_valid):
-        os.remove(args.save_valid)
+    # mecab = Mecab(args.mecab_dic)
+    # count = 0
+    # if os.path.exists(args.save_train):
+    #     os.remove(args.save_train)
+    # if os.path.exists(args.save_valid):
+    #     os.remove(args.save_valid)
 
     lines = open(args.corpus, 'r', encoding='utf-8').readlines()
     random.shuffle(lines)  # 順序をシャッフル
 
-    mecab_lines = Parallel(n_jobs=-1)([delayed(clean_and_tag)(line, args.mecab_dic) for line in tqdm(lines)])
+    labeled_data = Parallel(n_jobs=-1)([delayed(make_labeled_sentence)(line, args) for line in tqdm(lines)])
 
-    for words, parts in tqdm(mecab_lines):
-        # line = clean_text(line.rstrip())  # クリーニング
-        # words, parts = mecab.tagger(line)  # 形態素解析
-        words, parts = mecab.preprocessing_to_particle(words, parts, TARGETS, TARGET_PARTS)  # 2単語になった助詞を1単語に変換しておく
-        target_idx = get_target_positions(words, parts)  # 助詞の位置を検出
-        del_idx = get_del_positions(words, parts)  # 削除ラベルを挿入する位置を検出
-        n_target = len(target_idx) + len(del_idx)
+    with open(args.save_valid, 'w') as f:
+        for s in labeled_data[:args.valid_size]:
+            f.write(s + '\n')
 
-        # ラベル付けする位置を決める
-        if n_target == 0 or len(words) > args.maxlen:
-            continue
-        elif len(target_idx) == 0:
-            target_id = random.choice(del_idx)
-        elif len(del_idx) == 0:
-            target_id = random.choice(target_idx)
-        else:
-            # 文中に複数対象がある場合はランダムに1箇所選ぶ
-            # 削除ラベルをX%の確率で作成する
-            if random.random() < float(args.del_rate):
-                target_id = random.choice(del_idx)
-            else:
-                target_id = random.choice(target_idx)
+    with open(args.save_train, 'w') as f:
+        for s in labeled_data[args.valid_size:]:
+            f.write(s + '\n')
 
-        # ラベル付け
-        if parts[target_id][:2] == '助詞' or parts[target_id] == '助動詞':
-            marked_sentence = '{} <{}> {}'.format(
-                ' '.join(words[:target_id]), words[target_id], ' '.join(words[target_id+1:]))
-        else:
-            # 削除ラベルの場合
-            marked_sentence = '{} <{}> {}'.format(
-                ' '.join(words[:target_id]), 'DEL', ' '.join(words[target_id:]))
+    # for line in tqdm(lines):
+    #     line = clean_text(line.rstrip())  # クリーニング
+    #     words, parts = mecab.tagger(line)  # 形態素解析
+    #     words, parts = mecab.preprocessing_to_particle(words, parts, TARGETS, TARGET_PARTS)  # 2単語になった助詞を1単語に変換しておく
+    #     target_idx = get_target_positions(words, parts)  # 助詞の位置を検出
+    #     del_idx = get_del_positions(words, parts)  # 削除ラベルを挿入する位置を検出
+    #     n_target = len(target_idx) + len(del_idx)
 
-        save_path = args.save_valid if count < args.valid_size else args.save_train
-        open(save_path, 'a').write(marked_sentence + '\n')
-        count += 1
+    #     # ラベル付けする位置を決める
+    #     if n_target == 0 or len(words) > args.maxlen:
+    #         continue
+    #     elif len(target_idx) == 0:
+    #         target_id = random.choice(del_idx)
+    #     elif len(del_idx) == 0:
+    #         target_id = random.choice(target_idx)
+    #     else:
+    #         # 文中に複数対象がある場合はランダムに1箇所選ぶ
+    #         # 削除ラベルをX%の確率で作成する
+    #         if random.random() < float(args.del_rate):
+    #             target_id = random.choice(del_idx)
+    #         else:
+    #             target_id = random.choice(target_idx)
+
+    #     # ラベル付け
+    #     if parts[target_id][:2] == '助詞' or parts[target_id] == '助動詞':
+    #         marked_sentence = '{} <{}> {}'.format(
+    #             ' '.join(words[:target_id]), words[target_id], ' '.join(words[target_id+1:]))
+    #     else:
+    #         # 削除ラベルの場合
+    #         marked_sentence = '{} <{}> {}'.format(
+    #             ' '.join(words[:target_id]), 'DEL', ' '.join(words[target_id:]))
+
+    #     save_path = args.save_valid if count < args.valid_size else args.save_train
+    #     open(save_path, 'a').write(marked_sentence + '\n')
+    #     count += 1
 
 
 if __name__ == '__main__':
