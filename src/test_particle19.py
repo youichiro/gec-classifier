ab8aa2d61524759840dee0d7133c66baba38ad01
youichiro <cinnamon416@gmail.com>
2019-06-12 11:59:31 +0900

    fix

--- src/test_particle19.py
+++ src/test_particle19.py
@@ -12,6 +12,8 @@ def main():
     parser.add_argument('--ans', required=True, help='Answer corpus')
     parser.add_argument('--reverse', default=False, action='store_true',
                         help='Whether to reverse the prediction order')
+    parser.add_argument('--show', defalut=False, help='Whether to show results')
+    parser.add_argument('--save-dir', default='results/v2', help='Save directory')
     args = parser.parse_args()
 
     mecab_dict_file = os.environ['MECABDIC']
@@ -23,17 +25,28 @@ def main():
     error_data = open(args.err).readlines()
     answer_data = open(args.ans).readlines()
 
+    if args.save_dir:
+        save_dir = args.save_dir + '/' + args.model_dir
+        os.makedirs(save_dir, exist_ok=True)
+        f_hyp = open(save_dir + '/model.hyp', 'w')
+
     for i, (err, ans) in enumerate(zip(error_data, answer_data)):
         err = err.replace('\n', '')
         ans = ans.replace('\n', '')
         hyp = checker.correction(err)
 
-        print(f'--- {i} ---')
-        print(f'err: {err}')
-        print(f'hyp: {hyp}')
-        print(f'ans: {ans}')
-        print()
+        if args.show:
+            print(f'--- {i} ---')
+            print(f'err: {err}')
+            print(f'hyp: {hyp}')
+            print(f'ans: {ans}')
+            print()
 
+        if args.save_dir:
+            f_hyp.write(hyp + '\n')
+
+    if args.save_dir:
+        f_hyp.close()
 
 if __name__ == '__main__':
     main()
