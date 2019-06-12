434bb89bf43336cd3de5572068dde2b4f687e857
youichiro <cinnamon416@gmail.com>
2019-06-12 16:37:30 +0900

    fix

--- src/test_particle19.py
+++ src/test_particle19.py
@@ -53,7 +53,8 @@ def main():
 
     # m2scoreを実行
     cmd = f"python /lab/ogawa/tools/m2scorer/m2scorer {save_file_char} {args.save_dir}/naist_test19_char.m2 > {save_dir}/m2score.txt"
-    subprocess.run(cmd, shell=True)
+    result = subprocess.check_output(cmd, shell=True)
+    print(result)
 
 
 if __name__ == '__main__':
